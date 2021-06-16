a8967434f87f11aa7b8bd0af2d16eb46daaf3325
shihar <shiharbinsideeque73@gmail.com>
2021-06-17 04:07:05 +0530

    download module setup

--- download.py
+++ download.py
@@ -3,17 +3,33 @@ import urllib
 from urllib.parse import urlparse
 
 
-def dw_vid(url):
-    name = urlparse(url).path[-25:]
-    resource = urllib.request.urlopen(url)
-    f_name = "/root/Downloads/f/"+name
-    output = open(f_name, "wb")
-    output.write(resource.read())
-    output.close
-    print("video : ", f_name, 'download success')
+class insta_download:
+    # This is download videos and images use source url
+    def dw_vid(self, url, root):
+        try:
+            name = urlparse(url).path[-25:]
+            resource = urllib.request.urlopen(url)
+
+            if root:
+                f_name = root + name
+            else:
+                f_name = "/root/Downloads/" + name
+
+            print(f_name)
+            output = open(f_name, "wb")
+            output.write(resource.read())
+            output.close
+            print(f_name, 'download success')
+        except:
+            print("Error: Download Failed check url or location")
 
 
-url_a = str(input("Enter url : "))
-dw_vid(url_a)
+def main():
+    dw = insta_download()
+    loc = str(input("Enter location (Default : /root/Downloads/): "))
+    url_a = str(input("Enter url : "))
+    dw.dw_vid(url_a, loc)
 
 
+if __name__ == '__main__':
+    main()
